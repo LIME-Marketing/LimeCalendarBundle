3df5b7a97962abc755f23da90547cba90ad025f7
Derek Stobbe <djstobbe@gmail.com>
2011-09-26 18:05:20 -0600

    Add method signatures to CalendarInterface

--- Model/CalendarInterface.php
+++ Model/CalendarInterface.php
@@ -2,6 +2,8 @@
 
 namespace Lime\CalendarBundle\Model;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+
 interface CalendarInterface
 {
     
@@ -9,4 +11,26 @@ interface CalendarInterface
     const VISIBILITY_PRIVATE = 2;
     const VISIBILITY_PUBLIC = 4;
     
+    public function getId();
+
+    public function getCreatedAt();
+
+    public function getUpdatedAt();
+
+    public function setOwner(UserInterface $owner);
+
+    public function getOwner();
+
+    public function addParticipant(CalendarParticipantInterface $participant);
+
+    public function getParticipants();
+
+    public function addEvent(EventInterface $event);
+
+    public function getEvents();
+
+    public function setVisibility($visibility);
+
+    public function getVisibility();
+
 }
