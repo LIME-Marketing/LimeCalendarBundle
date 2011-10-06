c8a4bcc9fd67adef548b7ba69ebe8c446bf3456b
Derek Stobbe <djstobbe@gmail.com>
2011-10-06 15:01:29 -0600

    Add functional controllers for calendar and event

--- Controller/CalendarController.php
+++ Controller/CalendarController.php
@@ -3,46 +3,115 @@
 namespace Lime\CalendarBundle\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Lime\CalendarBundle\Model\CalendarManagerInterface;
+use Symfony\Component\Form\Form;
 
 class CalendarController extends ContainerAware
 {
 
-    public function createAction()
+    public function indexAction()
     {
+        $manager = $this->getCalendarManager();
+        $calendars = $manager->findAll();
+
+        return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Calendar:index.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
+
+        ));
+    }
+
+    public function createAction(Request $request)
+    {
+        $manager = $this->getCalendarManager();
+        $calendar = $manager->createCalendar();
+
+        $form = $this->getCalendarForm();
+        $form->setData($calendar);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid() && $manager->addCalendar($calendar)) {
+                return new RedirectResponse($this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.calendar.view'), array(
+                    'id' => $calendar->getId(),
+                )));
+            }
+        }
 
         return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Calendar:create.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
-
+            'form' => $form->createView(),
         ));
     }
 
     public function viewAction($id)
     {
-        $manager = $this->container->get('lime_calendar.calendar_manager');
+        $manager = $this->getCalendarManager();
         $calendar = $manager->find($id);
 
         return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Calendar:view.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
-
+            'calendar' => $calendar,
         ));
     }
 
-    public function editAction($id)
+    public function editAction($id, Request $request)
     {
-        $manager = $this->container->get('lime_calendar.calendar_manager');
+        $manager = $this->getCalendarManager();
         $calendar = $manager->find($id);
+
+        $form = $this->getCalendarForm();
+        $form->setData($calendar);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid() && $manager->updateCalendar($calendar)) {
+                return new RedirectResponse($this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.calendar.view'), array(
+                    'id' => $calendar->getId(),
+                )));
+            }
+        }
 
         return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Calendar:edit.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
-
+            'form' => $form->createView(),
         ));
     }
 
-    public function deleteAction($id)
+    public function deleteAction($id, Request $request)
     {
-        $manager = $this->container->get('lime_calendar.calendar_manager');
+        $manager = $this->getCalendarManager();
         $calendar = $manager->find($id);
 
-        return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Calendar:delete.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
+        if ('POST' === $request->getMethod()) {
+            $manager->removeCalendar($calendar);
 
+            return new RedirectResponse($this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.calendar.index'), array(
+
+            )));
+        }
+
+        return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Calendar:delete.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
+            'calendar' => $calendar,
+            'cancelURL' => $this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.calendar.view'), array(
+                'id' => $calendar->getId(),
+            )),
         ));
+    }
+
+    /**
+     * @return CalendarManagerInterface
+     */
+    protected function getCalendarManager()
+    {
+        return $this->container->get('lime_calendar.manager.calendar');
+    }
+
+    /**
+     * @return Form
+     */
+    protected function getCalendarForm()
+    {
+        return $this->container->get('lime_calendar.form_factory.calendar')->createForm();
     }
 
 }
--- Controller/EventController.php
+++ Controller/EventController.php
@@ -3,48 +3,127 @@
 namespace Lime\CalendarBundle\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
+use Lime\CalendarBundle\Model\EventManagerInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class EventController extends ContainerAware
 {
 
-    public function createAction()
+    public function indexAction()
     {
-        $manager = $this->container->get('lime_calendar.event_manager');
-        $event = $manager->createEvent();
+        $manager = $this->getEventManager();
+        $calendars = $manager->findAll();
+
+        return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Event:index.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
+
+        ));
+    }
+
+    /**
+     * @param integer $id id of calendar that event is being created for
+     */
+    public function createAction($id, Request $request)
+    {
+        $calManager = $this->container->get('lime_calendar.manager.calendar');
+        $calendar = $calManager->find($id);
+
+        $manager = $this->getEventManager();
+        $event = $manager->createEvent($calendar);
+
+        $form = $this->getEventForm();
+        $form->setData($event);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid() && $manager->addEvent($event)) {
+                return new RedirectResponse($this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.event.view'), array(
+                    'id' => $event->getId(),
+                )));
+            }
+        }
 
         return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Event:create.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
 
         ));
     }
 
+    /**
+     * @param integer $id id of event to be viewed
+     */
     public function viewAction($id)
     {
-        $manager = $this->container->get('lime_calendar.event_manager');
+        $manager = $this->getEventManager();
         $event = $manager->find($id);
 
         return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Event:view.html.' . $this->container->getParamter('lime_calendar.template.engine'), array(
-
+            'event' => $event,
         ));
     }
 
-    public function editAction($id)
+    /**
+     * @param integer $id id of event to be edited
+     */
+    public function editAction($id, Request $request)
     {
-        $manager = $this->container->get('lime_calendar.event_manager');
+        $manager = $this->getEventManager();
         $event = $manager->find($id);
+
+        $form = $this->getEventForm();
+        $form->setData($event);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid() && $manager->updateEvent($event)) {
+                return new RedirectResponse($this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.event.view'), array(
+                    'id' => $event->getId(),
+                )));
+            }
+        }
 
         return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Event:edit.html.' . $this->container->getParamter('lime_calendar.template.engine'), array(
-
+            'form' => $form->createView(),
         ));
     }
 
-    public function deleteAction($id)
+    /**
+     * @param integer $id id of event to be deleted
+     */
+    public function deleteAction($id, Request $request)
     {
-        $manager = $this->container->get('lime_calendar.event_manager');
+        $manager = $this->getEventManager();
         $event = $manager->find($id);
 
-        return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Event:delete.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
+        if ('POST' === $request->getMethod()) {
+            $manager->removeEvent($event);
 
+            return new RedirectResponse($this->container->get('router')->generate($this->container->getParameter('lime_calendar.routing.event.index'), array(
+
+            )));
+        }
+
+        return $this->container->get('templating')->renderResponse('LimeCalendarBundle:Event:delete.html.' . $this->container->getParameter('lime_calendar.template.engine'), array(
+            'event' => $event,
         ));
+    }
+
+    /**
+     * @return EventManagerInterface
+     */
+    protected function getEventManager()
+    {
+        return $this->container->get('lime_calendar.manager.event');
+    }
+
+    /**
+     * @return Form
+     */
+    protected function getEventForm()
+    {
+        return $this->container->get('lime_calendar.form_factory.event')->createForm();
     }
 
 }
